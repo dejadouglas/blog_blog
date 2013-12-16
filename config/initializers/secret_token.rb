ec1b3bcd5be1c77492f9d439eb35da986627ebf4
Clearra Bassett <cbassett@chicagotechacademy.org>
2013-12-16 13:23:19 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogBlog::Application.config.secret_token = 'a94bb357ccc445046e03f303cd903a8fa1bff06799171a9b9cb1ffae2b190b628172f895cf66b01d9c57d64076ee0d72ac88cc084040474019ef4829a5ed1b9f'
